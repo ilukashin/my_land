b04fb35f128ca62f894688709e3af107f3b5e7fe
gregoryawesoman <ray_couper@mail.ru>
2019-03-31 19:23:25 +0300

    restyling, added contacts page

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
 
   get 'about', to: 'pages#about'
   get 'works', to: 'pages#works'
+  get 'contacts', to: 'pages#contacts'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
